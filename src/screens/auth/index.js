6a91206eacb82132cd197573422061c791ba6d34
Tamara Antonella Frazzetta <tamarafrazzetta@gmail.com>
2022-10-26 14:16:09 -0300

    auth screen styles

--- src/screens/auth/index.js
+++ src/screens/auth/index.js
@@ -4,22 +4,65 @@ import {
   Text,
   TouchableOpacity,
   KeyboardAvoidingView,
+  TextInput,
 } from "react-native";
 import { styles } from "./styles";
+// import {colors
+import { colors } from "../../constants/themes";
 
 const Auth = ({ navigation }) => {
+  const title = "Registro";
+  const message = "¿Ya tienes una cuenta?";
+  const buttonTitle = "Iniciar sesión";
+  const messageAction = "Registrarse";
+  const messageTarget = "Ingresar ahora";
   return (
-    <KeyboardAvoidingView style={styles.container}>
-      <View style={styles.container}>
-        <Text style={styles.title}>Bienvenido!</Text>
+    <KeyboardAvoidingView behavior="padding" style={styles.container}>
+      <View style={styles.inputCont}>
+        <Text style={styles.title}>{title}</Text>
+        <View style={styles.inputs}>
+          <Text style={styles.label}>Email</Text>
+          <TextInput
+            style={styles.input}
+            placeholder="Ingrese su email"
+            placeholderTextColor={colors.gray}
+            autoCapitalize="none"
+            autoCorrect={false}
+            keyboardType="email-address"
+            onChangeText={(text) => {
+              console.log("text", text);
+            }}
+          ></TextInput>
+        </View>
+        <View>
+          <Text style={styles.label}>Contraseña</Text>
+          <TextInput
+            style={styles.input}
+            placeholder="Ingrese su contraseña"
+            placeholderTextColor={colors.gray}
+            autoCapitalize="none"
+            autoCorrect={false}
+            onChangeText={(text) => {
+              console.log("text", text);
+            }}
+            secureTextEntry={true}
+          ></TextInput>
+        </View>
         <TouchableOpacity
           style={styles.button}
-          onPress={() => navigation.navigate("Login")}
+          onPress={() => console.log("iniciar sesion")}
         >
-          <Text style={styles.buttonText}>Iniciar sesión</Text>
+          <Text style={styles.buttonText}>{messageAction}</Text>
         </TouchableOpacity>
+        <View style={styles.prompt}>
+          <Text>{message}</Text>
+          <TouchableOpacity onPress={() => console.log("Change")}>
+            <Text style={styles.promptMessage}>{messageTarget}</Text>
+          </TouchableOpacity>
+        </View>
       </View>
-    </KeyboardAvoidingView>)
+    </KeyboardAvoidingView>
+  );
 };
 
 export default Auth;
--- src/screens/auth/styles.js
+++ src/screens/auth/styles.js
@@ -1,14 +1,59 @@
 import { StyleSheet } from "react-native";
+import { colors } from "../../constants/themes";
 
 export const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+
     alignItems: "center",
-    justifyContent: "center",},
+    justifyContent: "center",
+  },
   title: {
     fontFamily: "Lato-Regular",
     fontSize: 20,
     marginBottom: 30,
+    color: colors.primary,
+    textAlign: "center",
   },
+  inputCont: {
+    width: "80%",
+    marginBottom: 20,
+    maxWidth: 400,
+    margin: 10,
+    backgroundColor: "#fff",
+    padding: 20,
+    borderRadius: 10,
+  },
+  label: {
+    fontFamily: "Lato-Regular",
+    fontSize: 16,
+    marginVertical: 5,
+  },
+  input: {
+    height: 40,
+    borderBottomColor: colors.primary,
+    marginBottom: 10,
+    width: "90%",
+    borderBottomWidth: 1,
+  },
+  button: {
+    backgroundColor: colors.lightGrey,
+    padding: 10,
+    borderRadius: 5,
+    marginVertical: 10,
+  },
+  buttonText: {
+    textAlign: "center",
+    fontWeight: "bold",
+  },
+  prompt: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginVertical: 10,
+  },
+  promptMessage: {
+    color: colors.primary,
+
+  }
 });
